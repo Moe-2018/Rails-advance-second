70dab2c523e24298503550239bb064d7bea228ee
Moe-2018 <monamour.moeno0108@gmail.com>
2026-02-01 11:44:43 +0000

    Update flash and error message styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :fullname, presence: true, length: { maximum: 25 }
 end
